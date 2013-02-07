d959cf62a1f8bd9bd1a8331d943e5024c83512de
Ethan Clark <eclark@monkeyvoodoo.net>
2013-02-07 09:01:57 -0800

    Fix new suplex_include replies

--- replies/actions/suplex_include.php
+++ replies/actions/suplex_include.php
@@ -1,13 +1,9 @@
 <?php
 $reply = array(
-	"ACTION:blushes\nW- What are you doing?!",
-	"ACTION:turns bright red\nWhere are you touching me?!",
-	// "ACTION:blushes and promptly takes a seat on <nick>\n...\nACTION:jumps up\n...\nWhat's this ... sticky...",
-	"Oniichan, that feels... funny!",
-	"Don't tickle me- AHN!"
+	"ACTION:suplexes <randomnick>",
+	"ACTION:looks a bit disoriented\nW- What."
 );
 switch(strtolower($nick)) {
-	case 'argon': array_push($reply, "ACTION:looks worried\nYou rub me too much!"); break;
 	case 'monkey': $reply = array("<nick>, don't be dumb."); break;
 }
 ?>
