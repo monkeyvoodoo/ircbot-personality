c5df807fc0734c1890cd2ab674c3c861f37b9e15
Ethan Clark <eclark@monkeyvoodoo.net>
2013-02-08 11:01:00 -0800

    Add "regex" response

---
+++ replies/actions/regex.php
@@ -0,0 +1,19 @@
+<?php
+$action = array(
+	'type'      => 'action',
+	'name'      => 'regex',
+	'synonyms'  => array(
+		'regex',
+		"regex's",
+		'regexes'
+	),
+	'replies'   => array(
+		"0 matches! HAHA!\ns/<nick>/BAKA!/g HAHAHA!",
+		"/[b]{2}|[^b]{2}/, that is the question."
+	),
+	'declines'  => array(),
+	'friendly'  => false,
+	'points'    => 1,
+	'threshold' => 0
+);
+?>
